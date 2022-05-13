baf4cdfc6b191fa637b34b0b0d0d5fc4919d95bc
luamfb <luamfb@riseup.net>
2022-05-13 14:44:36 -0300

    Implement class AnalisadorDeAprovacao conforming to tests

--- src/main/kotlin/domain/AnalisadorDeAprovacao.kt
+++ src/main/kotlin/domain/AnalisadorDeAprovacao.kt
@@ -1,15 +1,18 @@
 package domain
 
+import domain.criterios.CriterioDeAprovacao
+
 class AnalisadorDeAprovacao {
 
-    // ---------------------------------
-    //
-    //      Seu código deve vir aqui
-    //
-    //      Defina atributos e métodos conforme especificado
-    //      no arquivo de teste encontrado em
-    //      'src/test/kotlin/domain/AnalisadorDeAprovacaoTest'
-    //
-    // ---------------------------------
+    private lateinit var criterio: CriterioDeAprovacao
 
+    fun defineCriterio(criterio: CriterioDeAprovacao) {
+        this.criterio = criterio;
+    }
+
+    fun fechaBoletim(boletim: Boletim): BoletimFechado {
+        var aprovado = this.criterio.estaAprovado(boletim)
+        var mediaFinal = this.criterio.mediaFinal(boletim)
+        return BoletimFechado(boletim.mediaEPs, boletim.mediaMiniEPs, mediaFinal, aprovado)
+    }
 }
